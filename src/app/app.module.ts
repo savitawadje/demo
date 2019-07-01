d37bbac7eaac08eb7504753cb34c98753755caf4
SAVITA wadje <savitawadje5@gmail.com>
2019-07-01 23:21:24 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+  ]
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
